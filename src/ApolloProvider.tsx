554ddef90440624a61a1df98f888dda14c0d842e
paradoxinfinite <10831629+ParadoxInfinite@users.noreply.github.com>
2021-05-27 00:24:20 +0530

    Updated broken libraries

--- src/ApolloProvider.tsx
+++ src/ApolloProvider.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import App from "./App";
-import ApolloClient from "apollo-client";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { createHttpLink } from "apollo-link-http";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 import { ApolloProvider } from "@apollo/react-hooks";
-import { setContext } from "apollo-link-context";
+import { setContext } from "@apollo/link-context";
 
-const httpLink = createHttpLink({
+const httpLink = new HttpLink({
   uri: `https://discussionforumapi.herokuapp.com`,
 });
 
